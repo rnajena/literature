0d91267dfd45ea87aebd7e4f7a8de9d1b451324f
Franziska Hufsky <fhufsky@dyn-0a230c57.fmi.uni-jena.de>
2018-11-29 15:20:05 +0100

    cleaner

---
+++ cleanbibfile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#remove encoding
+sed -E '/^% Encoding:/ d' $@.bib > tmp.bib
+mv tmp.bib $@.bib
+#remove unnecessary bibtex fields
+sed -E '/^  (owner|pii|revised|country|issn|citation-subset|completed|issn-linking|nlm-id|pubmodel|pubstatus|pmc|month|__markedentry|chemicals)/ d' $@.bib > tmp.bib
+mv tmp.bib $@.bib
+#remove comment
+sed -E '/^@Comment/ d' $@.bib > tmp.bib
+mv tmp.bib $@.bib
